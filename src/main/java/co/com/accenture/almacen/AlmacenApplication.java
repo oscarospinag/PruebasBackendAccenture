76f035d3c64433ba77d230ab2a2f052f580d68e4
oscar_ospina <oospina@swat-it.co>
2021-04-25 13:10:01 -0500

    Se crea proyecto prueba desarrollador backend

---
+++ src/main/java/co/com/accenture/almacen/AlmacenApplication.java
@@ -0,0 +1,13 @@
+package co.com.accenture.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlmacenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/accenture/almacen/AlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.accenture.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
